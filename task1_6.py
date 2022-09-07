093a3234036760d9ba433032d4b43eda0adfbac0
MUGnifisent <slobodianyarik2003@gmail.com>
2022-09-07 11:31:20 +0300

    Set up workspace

--- task1_6.py
+++ task1_6.py
@@ -1,2 +1,15 @@
+from main import create_personal_numbers
+from utils.task1_utils import *
+
+
+def task1_6(pn):
+    pass
+
+
 if __name__ == '__main__':
-    print("Hello World!")
+    personalNumbers, listedLetters = create_personal_numbers()
+    print(f"Букви, отримані з вашого імені:\n{listedLetters}")
+    print(f"Цифри, перетворені через конвертаційну таблицю з вибраних букв:\n{personalNumbers}")
+    splitter()
+    task1_6(personalNumbers)
+    
