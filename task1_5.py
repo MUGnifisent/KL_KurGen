72e850782a6225cf5145357f24b45ebc011de72c
MUGnifisent <slobodianyarik2003@gmail.com>
2022-09-07 13:19:01 +0300

    changed the structure

--- task1_5.py
+++ task1_5.py
@@ -1,7 +1,16 @@
+from main import create_personal_numbers
 from utils.task1_utils import *
+
 
 def task1_5(pn):
     number_str = f"{pn[0][0]}{pn[0][1]}{pn[7][0]}{pn[7][1]}"
     print(number_str)
     print(decimal_to_binary(int(number_str)))
-    
+
+
+if __name__ == '__main__':
+    personalNumbers, listedLetters = create_personal_numbers()
+    print(f"Букви, отримані з вашого імені:\n{listedLetters}")
+    print(f"Цифри, перетворені через конвертаційну таблицю з вибраних букв:\n{personalNumbers}")
+    splitter()
+    task1_5(personalNumbers)
